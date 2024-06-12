c34cb01af66015f9b82b17e7d16fe12eb0f5be42
W3ske <simonskowron2222@gmail.com>
2024-06-13 00:04:42 +0200

    selectListItem development

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,25 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+import { TamaguiProvider, createTamagui } from "@tamagui/core"; // or 'tamagui'
+import { config } from "@tamagui/config/v3";
+
+// you usually export this from a tamagui.config.ts file
+const tamaguiConfig = createTamagui(config);
+
+// make TypeScript type everything based on your config
+type Conf = typeof tamaguiConfig;
+declare module "@tamagui/core" {
+  // or 'tamagui'
+  interface TamaguiCustomConfig extends Conf {}
+}
+
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView>
-      <Stack screenOptions={{ headerShown: false }} />
-    </GestureHandlerRootView>
+    <TamaguiProvider config={tamaguiConfig}>
+      <GestureHandlerRootView>
+        <Stack screenOptions={{ headerShown: false }} />
+      </GestureHandlerRootView>
+    </TamaguiProvider>
   );
 }
---
+++ components/SelectListItem/SelectListItem.tsx
@@ -0,0 +1,84 @@
+import { Check, ChevronDown } from "@tamagui/lucide-icons";
+import { useMemo, useState } from "react";
+
+import {
+  Label,
+  Select,
+  Sheet,
+  YStack,
+  XStack,
+  PortalProvider,
+  Adapt,
+  SelectProps,
+} from "tamagui";
+
+interface ListItem {
+  id: number;
+  name: string;
+}
+
+type SelectListItemProps = {
+  items: ListItem[];
+};
+
+export function SelectListItem() {
+  const [val, setVal] = useState("Choose options");
+
+  const items = [
+    { id: 1, name: "Detective" },
+    { id: 2, name: "Medic" },
+    { id: 3, name: "Serial Killer" },
+    { id: 4, name: "Medium" },
+  ];
+
+  // Memoize the list items
+  const listItems = items.map((item, i) => (
+    <Select.Item index={i} key={item.id} value={item.name.toLowerCase()}>
+      <Select.ItemText fontFamily="unset">{item.name}</Select.ItemText>
+      <Select.ItemIndicator marginLeft="auto">
+        <Check size={16} />
+      </Select.ItemIndicator>
+    </Select.Item>
+  ));
+
+  return (
+    <PortalProvider>
+      <Select
+        value={val}
+        onValueChange={setVal}
+        disablePreventBodyScroll={false}
+      >
+        <Select.Trigger width={"98%"} iconAfter={ChevronDown}>
+          <Select.Value placeholder="Something" />
+        </Select.Trigger>
+        <Adapt when="sm" platform="touch">
+          <Sheet
+          // native={!!props.native}
+          // modal
+          // dismissOnSnapToBottom
+          // animationConfig={{
+          //   type: "spring",
+          //   damping: 20,
+          //   mass: 1.2,
+          //   stiffness: 250,
+          // }}
+          >
+            <Sheet.Frame>
+              <Sheet.ScrollView>
+                <Adapt.Contents />
+              </Sheet.ScrollView>
+            </Sheet.Frame>
+            <Sheet.Overlay
+            // animation="lazy"
+            // enterStyle={{ opacity: 0 }}
+            // exitStyle={{ opacity: 0 }}
+            />
+          </Sheet>
+        </Adapt>
+        <Select.Content>
+          <Select.Viewport>{listItems}</Select.Viewport>
+        </Select.Content>
+      </Select>
+    </PortalProvider>
+  );
+}
